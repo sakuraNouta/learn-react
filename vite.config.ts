1ed53e8348869763ec23cd773fc9f17ff43b1b63
sakuraNouta <urbo0403@163.com>
2022-04-21 16:19:49 +0800

    chore: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()]
+  plugins: [react(), WindiCSS()],
+  base: '/vite-react-ts-app/',
+  build: {
+    outDir: 'docs'
+  }
 });
