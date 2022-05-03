f2fda241aced8fba838daa9f04208079460dd3f9
sakuraNouta <urbo0403@163.com>
2022-05-03 19:50:59 +0800

    feat: dva入门课

--- packages/dva-app/src/models/products.js
+++ packages/dva-app/src/models/products.js
@@ -1,9 +1,23 @@
+const delay = t => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({});
+    }, t);
+  });
+};
+
 export default {
   namespace: 'products',
   state: [],
   reducers: {
     delete(state, { payload: id }) {
       return state.filter(item => item.id !== id);
+    }
+  },
+  effects: {
+    *deleteAfter1Second(action, { put }) {
+      yield delay(1000);
+      yield put({ type: 'delete', payload: action.payload });
     }
   }
 };
